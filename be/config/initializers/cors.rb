54af82b6086d4eb85506040935dd8324eda296ef
Sal Espinosa <sespinos@gmail.com>
2024-01-26 23:46:48 -0600

    Allow for CORS requests in dev environment

---
+++ be/config/initializers/cors.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins Rails.application.config.allowed_cors_origins
+    resource '*', headers: :any, methods: [:get, :post]
+  end
+end
